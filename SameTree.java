e451b5dd142f2f5f0c01e043beda870f1ce7861f
Saiyed Kounain <128212021+saiyedkounain@users.noreply.github.com>
2025-12-23 06:33:19 +0530

    Leetcode 100

---
+++ SameTree.java
@@ -0,0 +1,30 @@
+/** leetcode 100
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        // recursive calls
+        if(p==null && q==null){
+            return true;
+        }
+
+        if(p!=null && q==null || p==null && q!=null || p.val != q.val){
+            return false;
+        }
+
+        //check left subtree AND right subtree
+        return (isSameTree(p.left,q.left) &  isSameTree(p.right, q.right));
+    }
+}
